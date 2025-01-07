36f28d4bb0ec08965ac9550a216fd83bc35ae061
ines <akez.ines@gmail.com>
2025-01-07 17:44:45 +0100

    feat(env): Configure environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  api: {
+    baseUrl: 'https://customer-api.amigoscode.dev',
+    authUrl: 'api/v1/auth/login',
+    customerUrl: 'api/v1/customers'
+  },
+  websocket: {
+    url: 'http://localhost:8080/ws-alerts'
+  }
+};
