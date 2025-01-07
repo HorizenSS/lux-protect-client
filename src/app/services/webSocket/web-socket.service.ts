24af4efb9c34a2c56f03eeab045a19484109ac80
ines <akez.ines@gmail.com>
2025-01-07 17:31:53 +0100

    feat(services): Add WebSocket service, Real-time communication, Socket handling

---
+++ src/app/services/webSocket/web-socket.service.ts
@@ -0,0 +1,69 @@
+import { Injectable } from '@angular/core';
+import { RxStomp } from '@stomp/rx-stomp';
+import { environment } from 'src/environments/environment';
+import {BehaviorSubject, Observable, tap} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AlertResponseDto } from '../../models/alert.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WebSocketService {
+  private rxStomp: RxStomp;
+  private currentLocation$ = new BehaviorSubject<L.LatLng | null>(null);
+  private userId: number | null = null;
+
+  constructor() {
+    this.rxStomp = new RxStomp();
+    this.initializeWebSocket();
+  }
+
+  private initializeWebSocket() {
+    const userStr = localStorage.getItem('user');
+    if (!userStr) return;
+
+    const userData = JSON.parse(userStr);
+    const token = userData.token;
+    this.userId = userData.customerDTO.id;
+
+    this.rxStomp.configure({
+      brokerURL: 'ws://localhost:8080/ws-alerts',
+      connectHeaders: {
+        Authorization: `Bearer ${token}`
+      },
+      debug: (msg: string) => console.log(`[WebSocket] ${msg}`),
+      heartbeatIncoming: 0,
+      heartbeatOutgoing: 20000,
+      reconnectDelay: 5000
+    });
+
+    this.rxStomp.activate();
+  }
+
+  updateLocation(location: L.LatLng) {
+    const userStr = localStorage.getItem('user');
+    if (!userStr) return;
+    const userData = JSON.parse(userStr);
+    this.userId = userData.customerDTO.id;
+    this.currentLocation$.next(location);
+    this.rxStomp.publish({
+      destination: `/app/location/${this.userId}`,
+      body: JSON.stringify({
+        latitude: location.lat,
+        longitude: location.lng
+      })
+    });
+  }
+
+  subscribeToNearbyAlerts(): Observable<AlertResponseDto> {
+    console.log('Subscribing to nearby alerts');
+    return this.rxStomp.watch('/topic/nearby-alerts').pipe(
+      tap(message => console.log('Received alert:', message.body)),
+      map(message => JSON.parse(message.body))
+    );
+  }
+
+  disconnect() {
+    this.rxStomp.deactivate();
+  }
+}
